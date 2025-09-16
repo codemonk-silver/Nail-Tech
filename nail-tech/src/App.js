aeed5450745f010803316b3fa07c8fc7c73988ff
codemonk-silver <olajugbagbeeniola12@outlook.com>
2025-09-16 02:21:37 -0700

    landing third changes

--- nail-tech/src/App.js
+++ nail-tech/src/App.js
@@ -1,11 +1,22 @@
 import Navbar from './components/Navbar/navbar';
 import Hero from './components/Hero/hero';
+import Trend from './components/Trend/trend';
+import Feature from './components/Feature/feature';
+import Gallery from './components/Gallery/gallery';
+import About from './components/About/about';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Trend />
+      <Feature />
+      <Gallery />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   );
 }
